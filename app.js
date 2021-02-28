4a14a7a3365b89bbc1289db8058e7fce861198be
Thanos Kaloudis <38354865+Thanoskaloudis@users.noreply.github.com>
2021-02-28 19:44:46 +0200

    add compare logic (weight, height, diet)

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 function Dinosaur(dinoData) {
   this.species = dinoData.species;
-  this.weight = Math.round(dinoData.weight / 2.21); // kg
-  this.height = Math.round(dinoData.height / 0.39); // cm
+  this.weight = (dinoData.weight / 2.21).toFixed(3); // kg
+  this.height = (dinoData.height / 0.39).toFixed(3); // cm
   this.diet = dinoData.diet;
   this.place = dinoData.place;
   this.time = dinoData.time;
   this.fact = dinoData.fact;
-}
+};
 
-function Human(name, weight, height, diet) {
-  this.name = name;
-  this.weight = weight;
-  this.height = height;
-  this.diet = diet;
-}
+function Human(hunamData) {
+  this.name = hunamData.name;
+  this.weight = hunamData.weight;
+  this.height = hunamData.headers;
+  this.diet = hunamData.diet;
+};
 
-function fetchData() {
+let dinosArray = [];
+
+(function fetchData() {
+  const link = "./dino.json";
   const myInit = {
     method: "GET",
     headers: {
@@ -25,48 +28,67 @@ function fetchData() {
     cache: "default",
   };
 
-  let myRequest = new Request("./dino.json", myInit);
+  let myRequest = new Request(link, myInit);
   fetch(myRequest)
     .then((resp) => {
       return resp.json();
     })
     .then((data) => {
-      createDinosaurs(data);
+      createDinosaurs(data.Dinos);
     });
-}
+})();
 
-function createDinosaurs(data) {
-  const dinos = data.Dinos;
-  const dinoArray = [];
-
+function createDinosaurs(dinos) {
   dinos.forEach((dino) => {
-    dinoArray.push(new Dinosaur(dino));
+    dinosArray.push(new Dinosaur(dino));
   });
-}
+};
 
-// Create Human Object
-
-function createHuman() {
+function getHumanData() {
   const name = document.querySelector("#name").value;
-  const weight = parseInt(document.querySelector("#weight").value);
+  const weight = parseInt(document.querySelector("#weight").value) / 100;
   const height =
     parseInt(document.querySelector("#meters").value * 100) +
     parseInt(document.querySelector("#centimeters").value);
   const diet = document.querySelector("#diet").value;
-  const human = new Human(name, weight, height, diet);
-  console.log(human);
-}
 
-// Use IIFE to get human data from form
+  return {
+    name: name,
+    weight: weight,
+    height: height,
+    diet: diet,
+  };
+};
 
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
+function compare() {
+  const human = getHumanData();
+  dinosArray.forEach((dino) => {
+    compareWeight(human.name, human.weight, dino.weight, dino.species);
+    compareHeight(human.name, human.height, dino.height, dino.species);
+    compareDiet(human.diet, dino.diet);
+  });
+};
 
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
+function compareWeight(name, humanWeight, dinoWeight, species) {
+  const percentage = ((humanWeight * 100) / dinoWeight).toFixed(3);
+  const approximate = humanWeight > dinoWeight ? "more" : "less";
+  console.log(
+    name + " has " + percentage + "% " + approximate + " weight than " + species
+  );
+};
 
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
+function compareHeight(name, humanHeight, dinoHeight, species) {
+  const percentage = ((humanHeight * 100) / dinoHeight).toFixed(3);
+  const approximate = humanHeight > dinoHeight ? "taller" : "sorter";
+  console.log(
+    name + " is " + percentage + "% " + approximate + " than " + species
+  );
+};
+
+function compareDiet(humanDiet, dinoDiet) {
+  const isSameDiet = (humanDiet.toLowerCase() == dinoDiet);
+  console.log(isSameDiet);
+};
 
 // Generate Tiles for each Dino in Array
 
